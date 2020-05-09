8ad481cc895b918a195000921ebb3883cfb51ea0
Christian <ctingle18@gmail.com>
2020-05-09 13:11:35 -0500

    Fixed ports

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-const server = app.listen(80, () => {
+const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
 });
